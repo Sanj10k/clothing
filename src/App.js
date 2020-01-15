503a39485d1613766f17126df204887b0f315182
Sanjay Payiri <sanjaypayiri@gmail.com>
2020-01-15 10:43:10 +0530

    adding Signin component

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import './App.css';
 import Homepage from './components/Homepage/Homepage';
 import {Switch, Route} from 'react-router-dom';
 import Shop from './components/Shop/Shop';
+import Header from './components/Header/Header';
+import SignInandSignUp from './components/SignIn-and-SignUp/SignIn-and-SignUp';
 
 /* const HatsPage = () => {
   return (
@@ -14,9 +16,11 @@ import Shop from './components/Shop/Shop';
 function App() {
   return (
     <div>
+      <Header />
       <Switch>
         <Route exact path='/' component={Homepage} />
         <Route path='/shop' component={Shop} />
+        <Route path='/signin' component={SignInandSignUp} />
       </Switch>
     </div>
   );
---
+++ src/components/SignIn-and-SignUp/SignIn-and-SignUp.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import './SignIn-and-SignUp.scss';
+import SignIn from '../SignIn/SignIn';
+
+const SignInandSignUp = () => (
+    <div className='sign-in-and-sign-up'>
+        <SignIn />
+    </div>
+);
+
+export default SignInandSignUp;

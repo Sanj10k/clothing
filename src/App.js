24a76485f7acd61d7b16271f8ac6ba3679ec442f
Sanjay Payiri <sanjaypayiri@gmail.com>
2020-01-24 13:11:58 +0530

    Implemented Firebase utils, adding user authentication

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import {Switch, Route} from 'react-router-dom';
 import Shop from './components/Shop/Shop';
 import Header from './components/Header/Header';
 import SignInandSignUp from './components/SignIn-and-SignUp/SignIn-and-SignUp';
-import {auth} from './Firebase/FirebaseUtils';
+import {auth, createUserProfileDocument} from './Firebase/FirebaseUtils';
 
 class App extends React.Component{
   constructor(){
@@ -19,8 +19,19 @@ class App extends React.Component{
   unsubscribeFomAuth = null;
 
   componentDidMount(){
-    this.unsubscribeFomAuth = auth.onAuthStateChanged(user => {
-      this.setState({currentUser : user});
+    this.unsubscribeFomAuth = auth.onAuthStateChanged(async userAuth => {
+      if (userAuth) {
+        const userRef = await createUserProfileDocument(userAuth);
+        userRef.onSnapshot(snapShot => {
+          this.setState({
+            currentUser: {
+              id : snapShot.id,
+              ...snapShot.data()
+            }
+          }, () =>{console.log(this.state);});
+        })
+      }
+      this.setState({currentUser: userAuth})
     });
   }
 
--- src/components/SignIn-and-SignUp/SignIn-and-SignUp.js
+++ src/components/SignIn-and-SignUp/SignIn-and-SignUp.js
@@ -2,10 +2,11 @@ import React from 'react';
 
 import './SignIn-and-SignUp.scss';
 import SignIn from '../SignIn/SignIn';
-
+import SignUp from '../SignUp/SignUp';
 const SignInandSignUp = () => (
     <div className='sign-in-and-sign-up'>
         <SignIn />
+        <SignUp />
     </div>
 );
 
